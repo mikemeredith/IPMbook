5fe32f33a3c4a32dfd0c0da816c6557a3dee6ce6
mikemeredith <mike@mmeredith.net>
2021-12-10 16:55:08 +0800

    Roll up 0.1.3

--- R/onAttach.R
+++
@@ -1,8 +0,0 @@
-
-
-.onAttach <- function(libname, pkgname) {
-  version <- try(packageVersion('IPMbook'), silent=TRUE)
-  if(!inherits(version, "try-error"))
-    packageStartupMessage("This is IPMbook ", version,
-      ". For overview type ?IPMbook; for changes do news(p='IPMbook').")
-}
